92509850957c1a9767ab92ee3851d93ee89338c8
Raj Ugavekar <ugavekarraj@gmail.com>
2025-07-11 20:08:46 +0530

    Notifications fixes

--- src/components/clients/NotificationBtn.jsx
+++ src/components/clients/NotificationBtn.jsx
@@ -3,9 +3,14 @@
 import { initPush } from "@/lib/features/initPushNotifications";
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { useEffect, useRef } from "react";
+import { useUserContext } from "./ContextProvider";
 
 export default function NotificationButton() {
   const [permission, setPermission] = useState("default");
+
+  const { user } = useUserContext();
+  const bellRef = useRef(null);
 
   const handleSubscribe = async () => {
     if (!("Notification" in window)) {
@@ -23,9 +28,22 @@ export default function NotificationButton() {
     }
   };
 
+  const hasShownToastRef = useRef(false);
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && "Notification" in window && Notification.permission === "default" &&
+      bellRef.current && user?._id && !hasShownToastRef.current) {
+      bellRef.current.focus();
+      bellRef.current.scrollIntoView({ behavior: "smooth" });
+      toast("Click the 🔔 icon to enable task reminders");
+      hasShownToastRef.current = true;
+    }
+  }, [user]);
+
   return (
     <button
       onClick={handleSubscribe}
+      ref={bellRef}
       disabled={permission === "granted"}
       className={` transition ${
         permission === "granted" ? "hidden" : ""
